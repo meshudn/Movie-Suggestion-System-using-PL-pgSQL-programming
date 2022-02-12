5e964f68067b0b5df1719ae3702a024dad65b3ea
meshu deb nath <meshu.uiu@gmail.com>
2022-02-12 22:29:46 +0100

    backbon added

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* Project Name: Film Suggestion Database (FSDB)
+* Developed by Meshu Deb Nath
+*/
+require 'dbconnection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FSDB</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- Header -->
+    <div class="container bodyContainer">
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto">
+                <div class="header">
+                   <h4 class="headl">Film Suggestion Database</h2>
+                </div>
+                <ul class="list-group headerList">
+                    <li class="list-group-item"><a href="film-person">Manage Film Related Person</a></li>
+                    <li class="list-group-item"><a href="film">Manage Films</a></li>
+                    <li class="list-group-item"><a href="rating">Manage Film User Rating</a></li>
+                    <li class="list-group-item"><a href="user">User Area</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
